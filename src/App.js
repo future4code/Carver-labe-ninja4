9449907453f7bbf56b70db361e6009ea765dab2c
gutto-moura <gmourah1792@gmail.com>
2021-11-24 17:36:03 -0300

    implementação filtros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import React from 'react'
-import { AppContainer } from './components/AppContainer'
+//import { AppContainer } from './components/AppContainer';
+import {Filtro} from './components/Filtros';
 
-function App() {
-	return (
-        <AppContainer />
+export default class App extends React.Component {
+	state = {
+		valueMin: "",
+		valueMax: "",
+		search:"",
+	}
+
+	updateSearch = (e) => {
+		this.setState({search: e.target.value,});
+	}
+	updateValueMin = (e) => {
+		if (e.target.value < 0) {
+			this.setState({valueMin: "",});
+		  } else {
+			this.setState({valueMin: e.target.value,});
+		  }
+	}
+	updateValueMax = (e) => {
+		if (e.target.value < 0) {
+			this.setState({valueMax: "",});
+		  } else {
+			this.setState({valueMax: e.target.value,});
+		  }
+	}
+
+	render(){
+		let filterServices = []
+		const filtrosCriterios = this.state.listService
+		.filter(service => {
+			return service.name.toLowerCase().includes(this.state.search.toLowerCase());
+		})
+		.filter(service => {
+			return this.state.valueMin === "" || service.value >= this.state.valueMin;
+		})
+		.filter(service => {
+			return this.state.valueMax === "" || service.value <= this.state.valueMax;
+		})
+		.map(service => {
+			return filterServices.push(service);
+		})
+
+		return(
+			<Filtro 
+			valueMin={this.state.valueMin}
+			valueMax={this.state.valueMax}
+			search={this.state.search}
+			updateSearch={this.updateSearch}
+			updateValueMin={this.updateValueMin}
+			updateValueMax={this.updateValueMax}
+			/>
 	)
+	}
+
 }
 
-export default App
---
+++ src/components/Filtros/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+
+export function Filtro(props) {
+    return (
+        <div>
+            <input
+                placeholder="Titulo"
+                value={props.search} 
+                onChange={props.updateSearch}
+            />
+            <input
+                placeholder="Valor mínimo"
+                value={props.valueMin} 
+                onChange={props.updateValueMin}
+            />
+             <input
+                placeholder="Valor máximo"
+                value={props.valueMax} 
+                onChange={props.updateValueMax}
+            />
+             <input
+                //value={} onChange={}
+            />
+            <h2>{props.listService}</h2>
+        </div>
+
+
+
+
+    );
+}
