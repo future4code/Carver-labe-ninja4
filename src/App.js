93146a360eb933c42483fc9a57f6cad39fad4e7e
Juliana-Frossard <ju.frossard@hotmail.com>
2021-11-25 13:15:26 -0300

    inicio do home

--- src/App.js
+++ src/App.js
@@ -1,72 +1,52 @@
 import React from 'react'
-import TelaDeBusca from './components/TelaDeBusca'
-import TelaDeCadastro from './components/TelaDeCadastro'
-import TelaDeDetalhes from './components/TelaDeDetalhes'
-import Carrinho from './components/Carrinho'
-import Home from './components/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+// import TelaDeBusca from './components/TelaDeBusca'
+// import TelaDeCadastro from './components/TelaDeCadastro'
+// import TelaDeDetalhes from './components/TelaDeDetalhes'
+// import Carrinho from './components/Carrinho'
+import Home from './components/Home';
+import Header from './components/Header';
+import TelaDeBusca from './components/TelaDeBusca';
+// import Footer from './components/Footer';
 
 export default class App extends React.Component {
 	state = {
-		currentScreen: "home"
-	}
+        telaAtual: "home"
+    }
+    escolheTelaDeCadastro= () => {
+        switch (this.state.telaAtual) {
+            case "home" :
+                return <Home/>
+            case "cadastro":
+                return <TelaDeCadastro/>
+            default:
+                return <p>Erro! Página não encontrada.</p>    
+        }
 
-	trocarPagina = () => {
-		switch(this.state.currentScreen) {
-			case "home":
-				return <Home irParaHome={this.irParaHome}/>
-			case "cadastro":
-				return <TelaDeCadastro irParaCadastro={this.irParaCadastro}/>
-			case "busca":
-				return <TelaDeBusca irParaBusca={this.irParaBusca}/>
-			case "carrinho":
-				return <Carrinho irParaCarrinho={this.irParaCarrinho}/>
-			case "detalhes":
-				return <TelaDeDetalhes irParaDetalhes={this.irParaDetalhes}/>
-			default:
-				return <div>Página não encontrada</div>
-		}
-	}
+    }
+    mudarParaCadastro = () => {
+        this.setState ({telaAtual: "cadastro"})
+    }
+	escolheTelaDeBusca= () => {
+        switch (this.state.telaAtual) {
+            case "home" :
+                return <Home/>
+            case "busca":
+                return <TelaDeBusca/>
+            default:
+                return <p>Erro! Página não encontrada.</p>    
+        }
 
-	irParaCadastro = () => {
-		this.setState({
-			currentScreen: "cadastro"
-		})
-	}
-
-	irParaBusca = () => {
-		this.setState({
-			currentScreen: "busca"
-		})
-	}
-
-
-	irParaHome = () => {
-		this.setState({
-			currentScreen: "home"
-		})
-	}
-
-	irParaCarrinho = () => {
-		this.setState({
-			currentScreen: "carrinho"
-		})
-	}
-
-	irParaDetalhes = () => {
-		this.setState({
-			currentScreen: "detalhes"
-		})
-	}
-
-	render () {
+    }
+    mudarParaBusca = () => {
+        this.setState ({telaAtual: "busca"})
+    }
+	render() {
 		return (
 			<div>
-				<Header/>
-					{this.trocarPagina()}
-				<Footer/>
+				<Header />
+				<p>O talento certo, no momento certo.</p>
+				<Home mudarParaCadastro={this.mudarParaCadastro} mudarParaBusca={this.mudarParaBusca}/>
 			</div>
 		)
 	}
-}	
+}
