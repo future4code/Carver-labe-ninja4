b16c61ac95696203f26549820db24bf518549a31
Juliana-Frossard <ju.frossard@hotmail.com>
2021-11-26 16:52:15 -0300

    inicio da estilização na pagina de cadastro

--- src/components/TelaDeCadastro/StyledCadastro.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Inputs= styled.div`
-border: 2px solid #7867bf 
-display:flex;
-
-`
--- src/components/TelaDeCadastro/TelaDeCadastro.js
+++ src/components/TelaDeCadastro/TelaDeCadastro.js
@@ -1,90 +1,106 @@
 import Axios from 'axios';
 import React, { Component } from 'react'
-import {Inputs} from './StyledCadastro'
+import './TelaDeCadastro.css'
 
 
 export default class TelaDeCadastro extends Component {
-  
-state={
-  title:'',
-  description:'',
-  price:'',
-  dueDate:'',	
-  paymentMethods:[]
-};
 
-handleTitle=(e)=>{
-this.setState({title: e.target.value})
-};
+  state = {
+    title: '',
+    description: '',
+    price: '',
+    dueDate: '',
+    paymentMethods: []
+  };
 
-handleDescription=(e)=>{
-  this.setState({description: e.target.value})
-};
+  handleTitle = (e) => {
+    this.setState({ title: e.target.value })
+  };
 
-handlePrice=(e)=>{
-  this.setState({price: e.target.value})
-};
+  handleDescription = (e) => {
+    this.setState({ description: e.target.value })
+  };
 
-handleDueDate=(e)=>{
-  this.setState({dueDate: e.target.value})
-};
+  handlePrice = (e) => {
+    this.setState({ price: e.target.value })
+  };
 
-handlePaymente=(e)=>{
-  let value = Array.from(e.target.selectedOptions, option => option.value)
-  this.setState({ paymentMethods: value })
-};
+  handleDueDate = (e) => {
+    this.setState({ dueDate: e.target.value })
+  };
 
-fazerCadastro=()=>{
-  const body={
-    
+  handlePaymente = (e) => {
+    let value = Array.from(e.target.selectedOptions, option => option.value)
+    this.setState({ paymentMethods: value })
+  };
+
+  fazerCadastro = () => {
+    const body = {
+
       title: this.state.title,
       description: this.state.description,
-      price: Number (this.state.price),
-      paymentMethods :this.state.paymentMethods,
+      price: Number(this.state.price),
+      paymentMethods: this.state.paymentMethods,
       dueDate: this.state.dueDate
-  }
-  Axios.post(' https://labeninjas.herokuapp.com/jobs', body,{
-    headers: {
-      Authorization:"13dfeab9-2ccf-4951-9acd-0d66de76427d"
     }
-  }).then(()=>{
-    alert('Produto cadastrado com sucesso.')
-    this.setState({
-      title: "",
-      description: "",
-      price: "",
-      dueDate: "",
-      paymentMethods: []
-  })
-  }).catch(()=>{
-    alert('erro ao cadastrar produto')
-  })
-};  
+    Axios.post(' https://labeninjas.herokuapp.com/jobs', body, {
+      headers: {
+        Authorization: "13dfeab9-2ccf-4951-9acd-0d66de76427d"
+      }
+    }).then(() => {
+      alert('Produto cadastrado com sucesso.')
+      this.setState({
+        title: "",
+        description: "",
+        price: "",
+        dueDate: "",
+        paymentMethods: []
+      })
+    }).catch(() => {
+      alert('erro ao cadastrar produto')
+    })
+  };
 
   render() {
     return (
-      <Inputs>
-    <input placeholder={'Produto'}
-    value={this.state.title}
-    onChange={this.handleTitle}/>
-    <input placeholder={'Preço'}
-    value={this.state.price}
-    onChange={this.handlePrice}/>
-    <input placeholder={'Descrição'}
-    value={this.state.description}
-    onChange={this.handleDescription}/> 
-    <select  multiple value={this.state.paymentMethods} onChange={this.handlePaymente}>
-                    <option>Cartão de Crédito</option>
-                    <option>Cartão de Débito</option>
-                    <option>Boleto</option>
-                    <option>Pix</option>
-                </select>
-    <input placeholder={'Prazo'}
-    value={this.state.dueDate}
-    onChange={this.handleDueDate}
-    type='date'/>
-    <button onClick={this.fazerCadastro}>Cadastrar</button>
-    </Inputs>
-     
-    )}
+      <div className="Inputs">
+        <div className="container1">
+          <div className="containerNomePrecoPrazo">
+            
+            <p>Nome:</p>
+            <input placeholder={'Produto'}
+              value={this.state.title}
+              onChange={this.handleTitle} />
+            
+            <p>Valor:</p>
+            <input placeholder={'Preço'}
+              value={this.state.price}
+              onChange={this.handlePrice} />
+            <p>Prazo:</p>
+            <input placeholder={'Prazo'}
+              value={this.state.dueDate}
+              onChange={this.handleDueDate}
+              type='date' />
+          </div>
+          <div className="containerDescricaoEPagamento">
+          <textarea placeholder={'Descrição'}
+            value={this.state.description}
+            onChange={this.handleDescription} />
+            <br/>
+            <p className="p">Qual a forma de pagamento que aceita?</p>
+          <select className="" multiple value={this.state.paymentMethods} onChange={this.handlePaymente}>
+            <option>Cartão de Crédito</option>
+            <option>Cartão de Débito</option>
+            <option>Boleto</option>
+            <option>Pix</option>
+          </select>
+          
+          </div>
+        </div>
+ 
+        <button className="button" onClick={this.fazerCadastro}>Cadastrar</button>
+      </div>
+
+    )
+  }
 };
